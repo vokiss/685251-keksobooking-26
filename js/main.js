091ce8d0fc1d7516587db4ccd5345ed984ff99fa
vokiss <vokisskotov@gmail.com>
2022-06-06 16:05:20 +0300

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-let getRandom = function(min, max) {
-  if (min >= max || min < 0 || max < 0) { 
+function getRandom(min, max) {
+  if (min >= max || min < 0 || max < 0) {
     console.log('ОШИБКА! диапазон может быть только положительный, включая ноль, первый аргумент должен быть меньше второго');
-  } else { 
+  } else {
     return Math.round(Math.random() * (max - min)) + min;}
-}; 
-console.log(getRandom(1,100));
+}
+getRandom(1,100);
 
 function getRandomWithDigits(min, max, digits) {
-    if (min >= max  min < 0  max < 0) { 
-     console.log('ОШИБКА! диапазон может быть только положительный, включая ноль, первый аргумент должен быть меньше второго');
-    } else {
-   let x = Math.round(Math.random() * (max - min + 1)) + min;
-   return x.toFixed([digits]);}
- }; 
- console.log(getRandomWithDigits(1.00000,33.55555,5));
+  if (min >= max || min < 0 || max < 0) {
+    console.log('ОШИБКА! диапазон может быть только положительный, включая ноль, первый аргумент должен быть меньше второго');
+  } else {
+    const x = Math.round(Math.random() * (max - min + 1)) + min;
+    return x.toFixed([digits]);}
+}
+getRandomWithDigits(1.00000,33.55555,5);
