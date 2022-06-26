bff7bd80437f13610d0cc2c8016ef4233422ba36
vokiss <vokisskotov@gmail.com>
2022-06-26 16:50:03 +0300

    Отрисуй меня полностью (часть 1)

--- js/data.js
+++ js/data.js
@@ -4,14 +4,22 @@ import {getRandomArrayElement,
 } from './util.js';
 
 const titles = ['Аппартаменты','Квартира', 'Комната', 'Вилла'];
-const types = ['Дворец ', 'Квартира ', 'Дом ', 'Бунгало ', 'Отель'];
 const checkTimes = ['12:00', '13:00', '14:00'];
 const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const descriptions = ['Описание1', 'Описание2', 'Описание3'];
 const photos = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+const types = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const encodedTypes = {
+  palace: 'Дворец' ,
+  flat:'Квартира',
+  house: 'Дом',
+  bungalow: 'Бунгало',
+  hotel: 'Отель'
+};
 
-const createBookingAd = (item, index) => {
+const createBookingAd = (_item, index) => {
   const location = [getRandomPositiveFloat(35.65000,35.70000,5), getRandomPositiveFloat(139.70000,139.80000,5)];
+
   return {
     location: {
       lat: location[0], //+число с плавающей точкой — широта, случайное значение от 35.65000 до 35.70000.
@@ -24,7 +32,7 @@ const createBookingAd = (item, index) => {
       title: getRandomArrayElement(titles), // +строка — заголовок предложения
       address: `${location[0]}, ${location[1]}` , // +строка — адрес предложения
       price: getRandomPositiveInteger(100,10000), // +число стоимость. Случайное целое положительное число.
-      type: getRandomArrayElement(types), // +строка — одно из пяти фиксированных значений
+      type: encodedTypes[getRandomArrayElement(types)], // +строка — одно из пяти фиксированных значений
       rooms: getRandomPositiveInteger(1,10), // +число — количество комнат. Случайное целое положительное число.
       guests: getRandomPositiveInteger(1,10), // +число — количество гостей, которое можно разместить. Случайное целое положительное число.
       checkin: getRandomArrayElement(checkTimes), //+ строка — одно из трёх фиксированных значений
--- js/form.js
+++ js/form.js
@@ -1,28 +1,66 @@
 import {createBookingAd} from './data.js';
 
-const similarCard = Array.from({length: 1}, createBookingAd);
+const similarCards = Array.from({length: 2}, createBookingAd);
 
 const cardTemplate = document.querySelector('#card')
   .content
   .querySelector('.popup');
 
 const mapCanvas = document.querySelector('#map-canvas');
-//mapCanvas.appendChild(cardTemplate);
 
-similarCard.forEach(({author, offer}) => {
+const spawnCard = function (data) {
   const cardElement = cardTemplate.cloneNode(true);
 
-  cardElement.querySelector('.popup__title').textContent = offer.title;
-  cardElement.querySelector('.popup__text--address').textContent = offer.address ;
-  cardElement.querySelector('.popup__text--price').textContent = `${offer.price  }₽/ночь`;
-  cardElement.querySelector('.popup__type').textContent = offer.type;
-  cardElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
-  cardElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
-  cardElement.querySelector('.popup__features').textContent = offer.features;
-  cardElement.querySelector('.popup__description').textContent = offer.description;
-  cardElement.querySelector('.popup__photo').src = offer.photos[0];
-  cardElement.querySelector('.popup__avatar').src = author.avatar;
-
-
+  const popupTitle = cardElement.querySelector('.popup__title');
+  popupTitle.textContent = data.offer.title;
+  if (popupTitle.textContent === undefined) {popupTitle.classList.add('hidden');}
+  const popupTextAdress = cardElement.querySelector('.popup__text--address');
+  popupTextAdress.textContent = data.offer.address;
+  if (popupTextAdress.textContent === undefined) {popupTextAdress.classList.add('hidden');}
+  const popupTextPrice = cardElement.querySelector('.popup__text--price');
+  popupTextPrice.textContent = `${data.offer.price  }₽/ночь`;
+  if (popupTextPrice.textContent === undefined) {popupTextPrice.classList.add('hidden');}
+  const popupType = cardElement.querySelector('.popup__type');
+  popupType.textContent = data.offer.type;
+  if (popupType.textContent === undefined) {popupType.classList.add('hidden');}
+  const popupText = cardElement.querySelector('.popup__text--capacity');
+  popupText.textContent = `${data.offer.rooms} комнаты для ${data.offer.guests} гостей`;
+  if (popupText.textContent === undefined) {popupText.classList.add('hidden');}
+  const popupTextTime = cardElement.querySelector('.popup__text--time');
+  popupTextTime.textContent = `Заезд после ${data.offer.checkin}, выезд до ${data.offer.checkout}`;
+  if (popupTextTime.textContent === undefined) {popupTextTime.classList.add('hidden');}
+  const popupFeatures = cardElement.querySelector('.popup__features');
+  popupFeatures.textContent = data.offer.features;
+  if (popupFeatures.textContent === undefined) {popupFeatures.classList.add('hidden');}
+  const popupDescription = cardElement.querySelector('.popup__description');
+  popupDescription.textContent = data.offer.description;
+  if (popupDescription.textContent === undefined) {popupDescription.classList.add('hidden');}
+  const popupPhoto = cardElement.querySelector('.popup__photo');
+  popupPhoto.src = data.offer.photos[0];
+  if (popupPhoto.textContent === undefined) {popupPhoto.classList.add('hidden');}
+  const popupAvatar = cardElement.querySelector('.popup__avatar');
+  popupAvatar.src = data.author.avatar;
+  if (popupAvatar.textContent === undefined) {popupAvatar.classList.add('hidden');}
   mapCanvas.appendChild(cardElement);
-});
+};
+
+similarCards.forEach((element) => spawnCard(element));
+
+
+const changeState = function (state) {
+  const adForm = document.querySelector('.ad-form');
+  const mapFilters = document.querySelector('.map__filters');
+  if (state === 0) {
+    adForm.classList.add('ad-form--disabled');
+    adForm.setAttribute('disabled', 'disabled');
+    mapFilters.classList.add('map__filters--disabled');
+    mapFilters.setAttribute('disabled', 'disabled');
+  }  else if (state === 1) {
+    adForm.classList.remove('ad-form--disabled');
+    adForm.removeAttribute('disabled');
+    mapFilters.classList.remove('map__filters--disabled');
+    mapFilters.removeAttribute('disabled');
+
+  }};
+
+changeState(1);
