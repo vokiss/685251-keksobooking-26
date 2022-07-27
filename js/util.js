ac7769afef3648ac8ee74c3bdef2ba1b686685f9
vokiss <103938789+vokiss@users.noreply.github.com>
2022-07-27 19:52:23 +0300

    Update util.js

--- js/util.js
+++ js/util.js
@@ -20,17 +20,6 @@ function changeState(isDisabled) {
     mapFilters.classList.toggle('ad-form--disabled');
     mapFilters.toggleAttribute('disabled');
   }
-  // if (state === 0) {
-  //   adForm.classList.add('ad-form--disabled');
-  //   adForm.setAttribute('disabled', 'disabled');
-  //   mapFilters.classList.add('map__filters--disabled');
-  //   mapFilters.setAttribute('disabled', 'disabled');
-  // }  else if (state === 1) {
-  //   adForm.classList.remove('ad-form--disabled');
-  //   adForm.removeAttribute('disabled');
-  //   mapFilters.classList.remove('map__filters--disabled');
-  //   mapFilters.removeAttribute('disabled');
-  // }
 }
 
 const blockSubmitButton = () => {
